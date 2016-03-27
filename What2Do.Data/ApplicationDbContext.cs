8ef9c9a6a7a77259dfb3e3470a2377637fa30836
D-Vault <rossmcarthur97@yahoo.com>
2016-03-27 14:27:45 +0100

    Class Library Added

---
+++ What2Do.Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace What2Do.Data
+{
+
+    /// <summary>
+    /// Connects to the database source
+    /// </summary>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("What2Do", throwIfV1Schema: false)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new database for the system
+        /// </summary>
+        /// <returns>New Database</returns>
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
