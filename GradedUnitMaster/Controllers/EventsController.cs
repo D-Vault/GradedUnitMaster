f5abbdbd46915da893632d6a723a1b3bb97bcdff
D-Vault <rossmcarthur97@yahoo.com>
2016-04-06 19:01:22 +0100

    Beginging event listing

--- GradedUnitMaster/Controllers/EventsController.cs
+++ GradedUnitMaster/Controllers/EventsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GradedUnitMaster.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,16 +9,38 @@ namespace GradedUnitMaster.Controllers
 {
     public class EventsController : MainController
     {
-        // GET: Events
+       /// <summary>
+       /// Displays a list of events within the system
+       /// </summary>
+       /// <returns>the view along with the list of events</returns>
         public ActionResult Index()
         {
             
+            var events = db.Events;
+
+            //Gets all entries in list and transfers them to the view model
+            var eventList = from u in events
+                            select new EventViewModel()
+                            {
+
+                                Id = u.EventID,
+                                Business = u.Business,
+                                capacity = u.Capacity, 
+                                name = u.EventName, 
+                                description = u.Description, 
+                                location = u.Location, 
+                                //dates = u.EventDates, 
+                                price = u.EventPrice, 
+                                type = u.Type, 
+                                restrictions = u.Restrictions
+                                
+                            };
 
 
-            return View();
+            return View(eventList.ToList());
         }
 
-        // GET: Events/Details/5
+         // GET: Events/Details/5
         public ActionResult Details(int id)
         {
             return View();
